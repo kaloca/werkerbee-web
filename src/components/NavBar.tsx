2b8ca25b7b90edb4302dd072aaaf43ef2a2e09d0
kaloca <mega.kaloca@gmail.com>
2023-05-24 17:10:41 -0700

    added logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from 'react'
 import { signIn, signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { useRouter, usePathname } from 'next/navigation'
+import Image from 'next/image'
+
+import WerkerBeeLogo from '../assets/werkerbeelogo.png'
 
 const selected = 'text-indigo-700 tracking-normal border-b-2 border-indigo-700'
 const normal = 'text-gry-800 tracking-normal'
@@ -29,12 +32,6 @@ export default function NavBar() {
 			isSelected: false,
 			url: '/create-job-posting',
 			type: 'company',
-		},
-		{
-			name: 'Home	',
-			isSelected: false,
-			url: '/jobs',
-			type: 'all',
 		},
 		{
 			name: 'Manage Posts',
@@ -502,7 +499,7 @@ export default function NavBar() {
 						<div className='container px-6 justify-between h-16 flex items-center lg:items-stretch mx-auto'>
 							<div className='h-full flex items-center'>
 								<div className='mr-10 flex items-center'>
-									<svg
+									{/* <svg
 										aria-label='Home'
 										id='logo'
 										enableBackground='new 0 0 300 300'
@@ -518,10 +515,16 @@ export default function NavBar() {
 												d='m234.735 35.532c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16zm0 24c-4.412 0-8-3.588-8-8s3.588-8 8-8 8 3.588 8 8-3.588 8-8 8zm-62.529-14c0-2.502 2.028-4.53 4.53-4.53s4.53 2.028 4.53 4.53c0 2.501-2.028 4.529-4.53 4.529s-4.53-2.027-4.53-4.529zm89.059 60c0 2.501-2.028 4.529-4.53 4.529s-4.53-2.028-4.53-4.529c0-2.502 2.028-4.53 4.53-4.53s4.53 2.029 4.53 4.53zm-40.522-5.459-88-51.064c-1.242-.723-2.773-.723-4.016 0l-88 51.064c-1.232.715-1.992 2.033-1.992 3.459v104c0 1.404.736 2.705 1.938 3.428l88 52.936c.635.381 1.35.572 2.062.572s1.428-.191 2.062-.572l88-52.936c1.201-.723 1.938-2.023 1.938-3.428v-104c0-1.426-.76-2.744-1.992-3.459zm-90.008-42.98 80.085 46.47-52.95 31.289-23.135-13.607v-21.713c0-2.209-1.791-4-4-4s-4 1.791-4 4v21.713l-26.027 15.309c-1.223.719-1.973 2.029-1.973 3.447v29.795l-52 30.727v-94.688zm0 198.707-80.189-48.237 51.467-30.412 24.723 14.539v19.842c0 2.209 1.791 4 4 4s4-1.791 4-4v-19.842l26.027-15.307c1.223-.719 1.973-2.029 1.973-3.447v-31.667l52-30.728v94.729z'
 											/>
 										</g>
-									</svg>
-									<h3 className='text-base text-gray-800 font-bold tracking-normal leading-tight ml-3 hidden lg:block'>
+									</svg> */}
+									<Image
+										src={WerkerBeeLogo}
+										width={150}
+										height={20}
+										alt={'logo'}
+									/>
+									{/* <h3 className='text-base text-gray-800 font-bold tracking-normal leading-tight ml-3 hidden lg:block'>
 										WerkerBee
-									</h3>
+									</h3> */}
 								</div>
 								<ul className='pr-12 flex flex-row justify-center h-full'>
 									{navBarItems.map((item) => {
