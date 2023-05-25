fb947a5146177a3a1f8f2f0684194f560fb95868
kaloca <mega.kaloca@gmail.com>
2023-05-25 16:11:56 -0700

    temporary image url

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+	experimental: {
+		appDir: true,
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'tuk-cdn.s3.amazonaws.com',
+				port: '',
+				pathname: '/assets/**',
+			},
+		],
+	},
 }
 
 module.exports = nextConfig
