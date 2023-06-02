417beb60cc056883d10023c969ccd9ca7d4cb48c
kaloca <mega.kaloca@gmail.com>
2023-06-02 14:23:49 -0700

    added production url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,5 @@
-export const BASE_URL = 'http://localhost:3000'
+process.env.NEXT_PUBLIC_GOOGLEMAPS_KEY as string
+export const BASE_URL =
+	process.env.NODE_ENV == 'development'
+		? 'http://localhost:3000'
+		: 'https://werkerbeeapi.onrender.com'
