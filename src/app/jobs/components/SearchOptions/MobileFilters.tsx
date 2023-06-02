d24c9d16e74a5e18230c2c1960f8122d1bfd93af
Gabriel Noya <mega.kaloca@gmail.com>
2023-06-02 14:42:12 -0700

    temporary type fi

--- src/app/jobs/components/SearchOptions/MobileFilters.tsx
+++ src/app/jobs/components/SearchOptions/MobileFilters.tsx
@@ -58,7 +58,7 @@ const MobileFilter: React.FC<MobileFilterProps> = ({
 							<form className='mt-4 border-t border-gray-200'>
 								<h3 className='sr-only'>Categories</h3>
 								<ul role='list' className='px-2 py-3 font-medium text-gray-900'>
-									{subCategories.map((category) => (
+									{subCategories.map((category: any) => (
 										<li key={category.name}>
 											<a href={category.href} className='block px-2 py-3'>
 												{category.name}
@@ -67,7 +67,7 @@ const MobileFilter: React.FC<MobileFilterProps> = ({
 									))}
 								</ul>
 
-								{filters.map((section) => (
+								{filters.map((section: any) => (
 									<Disclosure
 										as='div'
 										key={section.id}
@@ -97,27 +97,29 @@ const MobileFilter: React.FC<MobileFilterProps> = ({
 												</h3>
 												<Disclosure.Panel className='pt-6'>
 													<div className='space-y-6'>
-														{section.options.map((option, optionIdx) => (
-															<div
-																key={option.value}
-																className='flex items-center'
-															>
-																<input
-																	id={`filter-mobile-${section.id}-${optionIdx}`}
-																	name={`${section.id}[]`}
-																	defaultValue={option.value}
-																	type='checkbox'
-																	defaultChecked={option.checked}
-																	className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
-																/>
-																<label
-																	htmlFor={`filter-mobile-${section.id}-${optionIdx}`}
-																	className='ml-3 min-w-0 flex-1 text-gray-500'
+														{section.options.map(
+															(option: any, optionIdx: any) => (
+																<div
+																	key={option.value}
+																	className='flex items-center'
 																>
-																	{option.label}
-																</label>
-															</div>
-														))}
+																	<input
+																		id={`filter-mobile-${section.id}-${optionIdx}`}
+																		name={`${section.id}[]`}
+																		defaultValue={option.value}
+																		type='checkbox'
+																		defaultChecked={option.checked}
+																		className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
+																	/>
+																	<label
+																		htmlFor={`filter-mobile-${section.id}-${optionIdx}`}
+																		className='ml-3 min-w-0 flex-1 text-gray-500'
+																	>
+																		{option.label}
+																	</label>
+																</div>
+															)
+														)}
 													</div>
 												</Disclosure.Panel>
 											</>
