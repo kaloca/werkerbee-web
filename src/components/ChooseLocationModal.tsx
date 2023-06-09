7a53dffdb709e5308c3970bbbdf4d5c50d83bbd1
Gabriel Noya <mega.kaloca@gmail.com>
2023-06-09 04:58:31 -0700

    choose location ui fix

--- src/components/ChooseLocationModal.tsx
+++ src/components/ChooseLocationModal.tsx
@@ -25,16 +25,21 @@ const ChooseLocationModal: React.FC<ChooseLocationModalProps> = ({
 	return isOpen ? (
 		<div className='fixed inset-0 flex items-center justify-center z-50'>
 			<div className='absolute inset-0 bg-black opacity-50'></div>
-			<div className='bg-white p-6 rounded-lg z-50'>
-				<button onClick={onClose} className='text-right w-full'>
-					Close
-				</button>
+			<div className='bg-white px-6 pb-6 pt-3 rounded-lg z-50'>
+				<div className='w-full flex flex-row justify-end'>
+					<button
+						onClick={onClose}
+						className='text-right rounded hover:bg-gray-100 px-3 py-2'
+					>
+						Close
+					</button>
+				</div>
 				<input
 					type='text'
 					value={location}
 					onChange={handleLocationInput}
 					placeholder='Enter your location'
-					className='mt-4 p-2 border rounded'
+					className='mt-4 p-2 border rounded mr-3'
 				/>
 				<button
 					onClick={handleLocationConfirm}
