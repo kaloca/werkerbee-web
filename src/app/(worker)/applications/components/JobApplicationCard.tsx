f36d24561144e248f6aed39319edb86e7999ef8c
Gabriel Noya <mega.kaloca@gmail.com>
2023-06-09 02:54:06 -0700

    reworked applications screen

--- src/app/(worker)/applications/components/JobApplicationCard.tsx
+++ src/app/(worker)/applications/components/JobApplicationCard.tsx
@@ -1,21 +1,74 @@
+import { JobApplication } from '@/src/hooks/useApplications'
+import { JobPosting } from '@/src/hooks/useJobPostings'
+import helpers from '@/src/utils/helpers'
 interface JobApplicationCardProps {
-	application: any
+	application: JobApplication
+	onConfirmJobButton: (job: JobPosting, applicationId: string) => void
+	onClickStatus: (status: string, id: string) => void
+	onClickJobTitle: (id: string) => void
 }
 
 const JobApplicationCard: React.FC<JobApplicationCardProps> = ({
 	application,
+	onConfirmJobButton,
+	onClickStatus,
+	onClickJobTitle,
 }) => {
+	const { jobPosting, status, _id } = application
+
+	let statusColor
+	switch (status) {
+		case 'ACCEPTED':
+			statusColor = 'bg-green-100 text-green-700'
+			break
+		case 'REJECTED':
+			statusColor = 'bg-red-100 text-red-700'
+			break
+		case 'PENDING':
+			statusColor = 'bg-gray-100 text-gray-600'
+			break
+		case 'SCHEDULED':
+			statusColor =
+				'bg-blue-100 text-blue-700 hover:bg-blue-200 hover:cursor-pointer'
+	}
+
 	return (
-		<div className='w-full max-w-md  my-2 bg-white rounded-3xl shadow-xl overflow-hidden'>
+		<div className='w-full max-w-md  my-1 bg-white shadow-xl overflow-hidden'>
 			<div className='max-w-md mx-auto'>
 				<div className='p-4 sm:p-6'>
-					<p className='font-bold text-gray-700 text-[22px] leading-7 mb-1'>
-						{application.jobPosting.name}
+					<p
+						onClick={() => onClickJobTitle(jobPosting._id)}
+						className='font-bold text-gray-700 text-[22px] leading-7 mb-1 hover:text-blue-600 hover:underline hover:cursor-pointer'
+					>
+						{jobPosting.name}
 					</p>
-					<div className='flex flex-row'>
-						<p className='text-[#3C3C4399] text-[17px] mr-2 lowercase first-letter:capitalize'>
-							{application.status}
-						</p>
+					<div className='flex flex-row items-center justify-between mb-2'>
+						<span>
+							{jobPosting.dayOfWeek}
+							{' at '}
+							{helpers.formatAMPM(new Date(jobPosting.start))}
+						</span>
+						<span className='mr-5'>{jobPosting.payment}$/hr</span>
+					</div>
+					<div className='flex flex-row justify-between items-center'>
+						<div className='flex flex-row items-center'>
+							<div className={`h-4 w-4 rounded-2xl ${statusColor} mr-3`}></div>
+
+							<p
+								onClick={() => onClickStatus(status, '')}
+								className={`${statusColor} py-2 px-3 rounded font-semibold text-[15px] mr-2 lowercase first-letter:capitalize w-min`}
+							>
+								{status}
+							</p>
+						</div>
+						{status == 'ACCEPTED' && (
+							<button
+								onClick={() => onConfirmJobButton(jobPosting, _id)}
+								className=' bg-blue-500 hover:bg-blue-400 transition-colors duration-100 text-white rounded px-3 py-2'
+							>
+								Confirm Job
+							</button>
+						)}
 					</div>
 				</div>
 			</div>
--- src/app/(worker)/applications/page.tsx
+++ src/app/(worker)/applications/page.tsx
@@ -1,12 +1,34 @@
 'use client'
-import React from 'react'
-
-import useApplications from '@/src/hooks/useApplications'
-import JobApplicationCard from './components/JobApplicationCard'
+import React, { useState } from 'react'
+import { useSession } from 'next-auth/react'
 import { SyncLoader } from 'react-spinners'
 
+import apiClient from '@/src/utils/apiClient'
+
+import { JobPosting } from '@/src/hooks/useJobPostings'
+import useApplications, { JobApplication } from '@/src/hooks/useApplications'
+
+import AcceptDeclineModal from '@/src/components/AcceptDeclineModal'
+
+import JobApplicationCard from './components/JobApplicationCard'
+import ApplicationStatusSelector from './components/ApplicationStatusSelector'
+import ApplicationsHeading from './components/ApplicationsHeading'
+import { useRouter } from 'next/navigation'
+
 const Applications = () => {
-	const { data, error, isLoading } = useApplications()
+	const { data: session } = useSession()
+	const router = useRouter()
+
+	const [acceptJobModalOpen, setAcceptJobModalOpen] = useState(false)
+	const [confirmJobLoading, setConfirmJobLoading] = useState(false)
+	const [chosenJob, setChosenJob] = useState<{
+		jobPosting: JobPosting
+		applicationId: string
+	} | null>(null)
+	const [currentView, setCurrentView] = useState('View')
+
+	const { data, pagination, error, isLoading, setPage, refetch } =
+		useApplications()
 
 	if (error) {
 		return <div>Error: {error.message}</div>
@@ -14,27 +36,113 @@ const Applications = () => {
 
 	console.log(data)
 
+	const handleChangeView = (option: string) => {
+		setCurrentView(option)
+
+		const status =
+			option == 'All' || option == 'View' ? undefined : option.toUpperCase()
+
+		refetch(status)
+	}
+
+	const handleConfirmJobButton = (
+		jobPosting: JobPosting,
+		applicationId: string
+	) => {
+		setChosenJob({
+			jobPosting,
+			applicationId,
+		})
+		setAcceptJobModalOpen(true)
+	}
+
+	const handleClickStatus = (status: string, id: string) => {
+		if (status == 'SCHEDULED') router.push(`/calendar`)
+	}
+
+	const onConfirmJob = async () => {
+		if (session) {
+			try {
+				setConfirmJobLoading(true)
+				const response = await apiClient({
+					method: 'post',
+					url: `/job-application/${chosenJob?.applicationId}/confirm`,
+					token: session.user.token,
+				})
+
+				if (response?.status === 200) {
+					console.log('Job confirmed successfully')
+					// Copy the current applications
+					// let updatedApplications = [...applications]
+
+					// // Find the clicked application in the applications array and update its status
+					// for (let application of updatedApplications) {
+					// 	if (application._id === applicationId) {
+					// 		application.status = decision // or whatever status is appropriate here
+					// 	}
+					// }
+
+					// Update the state with the updated applications
+					refetch()
+				} else {
+					console.error(`Error confirming job: ${response.data.message}`)
+				}
+			} catch (error: any) {
+				console.error('Error confirming job:', error.response.data.message)
+				//showError(error.response.data.message)
+			}
+			setConfirmJobLoading(false)
+		}
+	}
+
 	return (
-		<div className='flex flex-col justify-center items-center h-full bg-slate-300'>
-			{!isLoading &&
-				(data && data.length > 0 ? (
-					data.map((application) => (
-						<JobApplicationCard
-							key={application._id}
-							application={application}
-						/>
-					))
-				) : (
-					<div>
-						<span>No current applications</span>
+		<div className='flex flex-col justify-start items-center h-full pt-20 bg-slate-200'>
+			<AcceptDeclineModal
+				title='Confirm job?'
+				subtitle={`Confirming this job will notify the company of your availability and add "${chosenJob?.jobPosting.name}" to your current jobs.`}
+				isOpen={acceptJobModalOpen}
+				setOpen={setAcceptJobModalOpen}
+				option1Title='Cancel'
+				option2Title='Confirm'
+				option2Action={onConfirmJob}
+				loading={confirmJobLoading}
+			/>
+			<div className='pb-4 mb-3 border-b w-1/3 border-gray-400 sm:flex sm:items-center sm:justify-between'>
+				<h3 className='text-lg leading-6 font-medium text-gray-900'>
+					Your Applications
+				</h3>
+				<ApplicationStatusSelector
+					onChoose={handleChangeView}
+					currentView={currentView}
+				/>
+			</div>
+			<div className='bg-blue-300'></div>
+			<div className='overflow-scroll no-scrollbar'>
+				{!isLoading &&
+					(data && data.length > 0 ? (
+						data.map((application) => (
+							<JobApplicationCard
+								key={application._id}
+								application={application}
+								onConfirmJobButton={handleConfirmJobButton}
+								onClickStatus={handleClickStatus}
+								onClickJobTitle={(id: string) =>
+									router.push(`/job-posting/${id}`)
+								}
+							/>
+						))
+					) : (
+						<div>
+							<span>No current applications</span>
+						</div>
+					))}
+				{isLoading && (
+					<div className='flex justify-center items-start'>
+						<h3>Loading applications</h3>
+						<SyncLoader size={8} className='m-4' />
 					</div>
-				))}
-			{isLoading && (
-				<div className='flex justify-center items-start'>
-					<h3>Loading applications</h3>
-					<SyncLoader size={8} className='m-4' />
-				</div>
-			)}
+				)}
+			</div>
 		</div>
 	)
 }
