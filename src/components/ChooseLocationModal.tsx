d8479d87b5332dc73cd5a965c985d4f6124ce229
Parth Mahawar <mahawarparth@gmail.com>
2023-07-17 15:35:52 -0700

    added location picker component, not finalized

--- src/components/ChooseLocationModal.tsx
+++ src/components/ChooseLocationModal.tsx
@@ -1,6 +1,9 @@
 import { Fragment, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
-import { CheckIcon } from '@heroicons/react/24/outline'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api"
+import axios from 'axios'
+
 
 type ChooseLocationModalProps = {
 	isOpen: boolean
@@ -17,6 +20,13 @@ const ChooseLocationModal: React.FC<ChooseLocationModalProps> = ({
 
 	const [location, setLocation] = useState<string>('')
 
+	const [searchText, setSearchText] = useState('')
+
+	const [selectedLocation, setSelectedLocation] = useState({
+		lat: 37.8,
+		lng: -122.3
+	})
+
 	const handleLocationInput = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setLocation(event.target.value)
 	}
@@ -25,6 +35,37 @@ const ChooseLocationModal: React.FC<ChooseLocationModalProps> = ({
 		onLocationSelect(location)
 		onClose()
 	}
+
+	const handleMapClick = (e) => {
+		setSelectedLocation({
+			lat: e.latLng.lat(),
+			lng: e.latLng.lng()
+		})
+	}
+
+	const handleCurrentLocation = (pos: GeolocationPosition) => {
+		setSelectedLocation({
+			lat: pos.coords.latitude,
+			lng: pos.coords.longitude
+		})
+	}
+
+	const handleSearch = () => {
+		var query = "https://nominatim.openstreetmap.org/search?q="
+					+ searchText + "&countrycodes=us" + "&limit=1"
+					+ "&format=json&viewbox=-122.6,38.2,-121.6,37.2"//Bias results to bay area
+		console.log(query)
+		axios.get(query)
+			.then(response => setSelectedLocation({
+				lat: parseFloat(response.data[0].lat),
+				lng: parseFloat(response.data[0].lon)
+			}))
+	}
+
+	const {isLoaded} = useJsApiLoader({
+		id: 'worker-location-picker',
+		googleMapsApiKey: (process.env.NEXT_PUBLIC_GOOGLEMAPS_KEY ?? "")
+	})
 
 	return (
 		<Transition.Root show={open} as={Fragment}>
@@ -36,7 +77,7 @@ const ChooseLocationModal: React.FC<ChooseLocationModalProps> = ({
 					setOpen(false)
 				}}
 			>
-				<div className='flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
+				<div className='flex items-end justify-center pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
 					<Transition.Child
 						as={Fragment}
 						enter='ease-out duration-300'
@@ -65,7 +106,7 @@ const ChooseLocationModal: React.FC<ChooseLocationModalProps> = ({
 						leaveFrom='opacity-100 translate-y-0 sm:scale-100'
 						leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
 					>
-						<div className='relative inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6'>
+						<div className='relative inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-xl sm:w-full sm:p-6'>
 							<div>
 								{/* <div className='mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100'>
 									<CheckIcon
@@ -73,19 +114,58 @@ const ChooseLocationModal: React.FC<ChooseLocationModalProps> = ({
 										aria-hidden='true'
 									/>
 								</div> */}
-								<div className='mt-3 text-center sm:mt-5'>
+								<div className='text-center sm:mt-2'>
 									<Dialog.Title
 										as='h3'
 										className='text-lg leading-6 font-medium text-gray-900'
 									>
 										Choose Your Location
 									</Dialog.Title>
-									<div className='mt-2'>
-										<p className='text-sm text-gray-500'>
-											Lorem ipsum dolor sit amet consectetur adipisicing elit.
-											Consequatur amet labore.
-										</p>
+									
+									<div className='mt-2'/>
+									<div className="flex rounded-md border border-gray-200">
+									<input 
+											type="text" 
+											className="rounded-l-md py-2 px-4 border-none text-gray-800 bg-white w-full" 
+											placeholder="Enter Address"
+											onChange={(e) => {setSearchText(e.target.value)}}
+										/>
+										<button 
+											className="rounded-r-md bg-gray-100 text-base font-medium text-indigo-700 hover:bg-gray-200 px-4"
+											onClick={handleSearch}
+										>
+											<MagnifyingGlassIcon className='h-6 w-6'/>
+										</button>					
 									</div>
+
+									<div className='mb-10 rounded-lg border mt-2'>
+										{ isLoaded ? (
+											<GoogleMap
+												mapContainerClassName='rounded-lg'
+												mapContainerStyle = {{
+													width: "100%",
+													height: "300px"
+												}}
+												zoom = {10}
+												center = {selectedLocation}
+												onClick = {handleMapClick}
+												>
+												<Marker
+													position={selectedLocation}
+												/>
+											</GoogleMap>)
+											: (<div></div>)
+										}
+									</div>
+									<button
+										type='button'
+										className='inline-flex justify-center w-full mr-2 rounded-md border border-transparent shadow-sm px-4 py-2 bg-gray-100 text-base font-medium text-indigo-700 hover:bg-gray-200 sm:text-sm'
+										onClick={() => {
+											navigator.geolocation.getCurrentPosition(handleCurrentLocation)
+										}}
+										>
+										Use current location
+									</button>
 								</div>
 							</div>
 							<div className='mt-5 sm:mt-6 grid grid-cols-2 gap-2'>
