476bebce86e4138932fd53739b3a708c5a09bde9
Gabriel Noya <mega.kaloca@gmail.com>
2023-07-30 14:57:57 +0300

    removed comments

--- src/app/(worker)/applications/components/JobApplicationCard.tsx
+++ src/app/(worker)/applications/components/JobApplicationCard.tsx
@@ -63,12 +63,6 @@ const JobApplicationCard: React.FC<JobApplicationCardProps> = ({
 							Expired
 						</span>
 					)}
-					{/* <div className='mt-1 flex items-center gap-x-1.5'>
-						<div className='flex-none rounded-full bg-emerald-500/20 p-1'>
-							<div className='h-1.5 w-1.5 rounded-full bg-emerald-500' />
-						</div>
-						<p className='text-xs leading-5 text-gray-500'>Online</p>
-					</div> */}
 				</div>
 			</div>
 			<div className='hidden sm:flex sm:flex-col sm:items-end'>
@@ -86,48 +80,6 @@ const JobApplicationCard: React.FC<JobApplicationCardProps> = ({
 				</p>
 			</div>
 		</li>
-		// <li className='bg-white shadow overflow-hidden rounded-md px-6 py-4'>
-		// 	{/* //<li className='w-full max-w-md  my-1 bg-white overflow-hidden'> */}
-		// 	{/* <div className='max-w-md mx-auto'>
-		// 		<div className='p-4 sm:p-6'> */}
-		// 	<p
-		// 		onClick={() => onClickJobTitle(jobPosting._id)}
-		// 		className='font-bold text-gray-700 text-[22px] leading-7 mb-1 hover:text-blue-600 hover:underline hover:cursor-pointer'
-		// 	>
-		// 		{jobPosting.name}
-		// 	</p>
-		// 	<div className='flex flex-row items-center justify-between mb-2'>
-		// 		<span>
-		// 			{jobPosting.dayOfWeek}
-		// 			{' at '}
-		// 			{helpers.formatAMPM(new Date(jobPosting.start))}
-		// 		</span>
-		// 		<span className='mr-5'>{jobPosting.payment}$/hr</span>
-		// 	</div>
-		// 	<div className='flex flex-row justify-between items-center'>
-		// 		<div className='flex flex-row items-center'>
-		// 			<div className={`h-4 w-4 rounded-2xl ${statusColor} mr-3`}></div>
-
-		// 			<p
-		// 				onClick={() => onClickStatus(status, '')}
-		// 				className={`${statusColor} py-2 px-3 rounded font-semibold text-[15px] mr-2 lowercase first-letter:capitalize w-min`}
-		// 			>
-		// 				{status}
-		// 			</p>
-		// 		</div>
-		// 		{status == 'ACCEPTED' && !expired && (
-		// 			<button
-		// 				onClick={() => onConfirmJobButton(jobPosting, _id)}
-		// 				className=' bg-blue-500 hover:bg-blue-400 transition-colors duration-100 text-white rounded px-3 py-2'
-		// 			>
-		// 				Confirm Job
-		// 			</button>
-		// 		)}
-		// 		{expired && <span className='text-red-500 mr-4'>Expired</span>}
-		// 	</div>
-		// 	{/* </div>
-		// 	</div> */}
-		// </li>
 	)
 }
 
