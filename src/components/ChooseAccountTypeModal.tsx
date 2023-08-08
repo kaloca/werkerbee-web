936bb654e5831d7d633e221e3ae7167204cf68f8
Gabriel Noya <mega.kaloca@gmail.com>
2023-08-08 15:09:37 +0300

    fixed mobile alignment on modals

--- src/components/ChooseAccountTypeModal.tsx
+++ src/components/ChooseAccountTypeModal.tsx
@@ -25,7 +25,7 @@ const AccountTypeModal: React.FC<AccountTypeModalProps> = ({
 				initialFocus={cancelButtonRef}
 				onClose={setOpen}
 			>
-				<div className='flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
+				<div className='flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
 					<Transition.Child
 						as={Fragment}
 						enter='ease-out duration-300'
